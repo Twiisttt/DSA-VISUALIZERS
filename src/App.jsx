7b7a8dd9d7ca601bdf0239a8f44090a3d80c784b
Twiisttt <rai.tejasvi0@gmail.com>
2026-08-06 23:47:09 +0530

    fix home and navbar issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,103 @@
-import ProtectedRoute from "./components/ProtectedRoute";
-import "./App.css";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import AlgorithmSection from "./components/AlgorithmSection";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 import Stack from "./pages/Stack";
 import Queue from "./pages/Queue";
 import Sorting from "./pages/Sorting";
 import Searching from "./pages/Searching";
 import Login from "./pages/Login";
-import Profile from "./pages/Profile";
 import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
+
+import "./App.css";
+
 
 function Home() {
   return (
     <div className="app">
-      <Navbar />
+
       <Hero />
+
       <AlgorithmSection />
+
     </div>
   );
 }
 
+
 function App() {
 
   return (
+
     <BrowserRouter>
+
+      {/* Navbar is now available on every page */}
+      <Navbar />
+
       <Routes>
 
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={<Home />}
+        />
 
-        <Route path="/stack" element={<Stack />} />
 
-        <Route path="/queue" element={<Queue />} />
+        <Route
+          path="/stack"
+          element={<Stack />}
+        />
 
-        <Route path="/sorting" element={<Sorting />} />
 
-        <Route path="/searching" element={<Searching />} />
+        <Route
+          path="/queue"
+          element={<Queue />}
+        />
 
-        <Route path="/login" element={<Login />} />
 
-        <Route path="/profile" element={<ProtectedRoute><Profile /> </ProtectedRoute>} />
+        <Route
+          path="/sorting"
+          element={<Sorting />}
+        />
 
-        <Route path="/signup" element={<Signup />}/>
+
+        <Route
+          path="/searching"
+          element={<Searching />}
+        />
+
+
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+
+
+        <Route
+          path="/signup"
+          element={<Signup />}
+        />
+
+
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
 
       </Routes>
+
     </BrowserRouter>
+
   );
+
 }
+
 
 export default App;
--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
-import API_URL from "../config/api";
+
 
 function ProtectedRoute({ children }) {
 
